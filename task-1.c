9325f4928b5fe61a534ed20b49b9de462df604c4
Sabbir Hossain <70355475+smsabbirHossen@users.noreply.github.com>
2023-11-13 21:30:54 +0600

    Update task.c

--- task-1.c
+++ task-1.c
@@ -1,43 +1,5 @@
-#include <stdio.h>
-int main(){
-
-
-    int x =10;
-    printf("The number of x:%d\n",x);
-    //print memory address
-    printf("Memory address of x:%d\n",&x);
-
-    // create pointer
-    int *ptr;
-    ptr =&x;
-
-    // print memory address with pointer
-    printf("Memory address of x use pointer:%d\n", ptr);
-    printf("The value of x with pointer:%d \n",*ptr);
-
-    //print pointer memory address
-    printf("pointer memory adderss:%d\n",&ptr);
-
-
-    //sum to variable value with pointer
-    int num1 = 20;
-    int num2= 30;
-
-
-    //create pointer
-    int *ptr1;
-    int *ptr2;
-
-    //store the memory address with pointer
-
-    ptr1 = &num1;
-    ptr2 = &num2;
-
-    int sum  = *ptr1 + *ptr2;
-    printf("Two number sum use pointer:%d\n",sum);
-
-
-
-return 0;
-
-}
+Name :Sabbir Hossain.
+id:22330091150.
+Program:BSC in CSE(Evening)
+Cousrs:Sturctured programming Language
+batch:60D(Fall 23)
